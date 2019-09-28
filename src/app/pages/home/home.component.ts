f2f46254b951835c6a6d78f822b5a790b8f17669
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-09-28 14:57:08 -0500

    force commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  epss: any[] = [
+    { Nombre: "Sanitas", Id: 1},
+    { Nombre: "Cafesalud", Id: 2 }
+  ];
+
   constructor() { }
 
   ngOnInit() {
